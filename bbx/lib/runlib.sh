0ab7d7d44293be3396b60270eecd0c478b8f9cdb
Johannes Dröge <johannes.droege@uni-duesseldorf.de>
2015-06-25 18:34:19 +0200

    changes for bioboxes refactoring init run code

--- bbx/lib/runlib.sh
+++ bbx/lib/runlib.sh
@@ -2,7 +2,7 @@ mountcheck() {
   grep -v '^#' "$BBX_MNTCONF" |
   while read ifc; do
     if ! mountpoint -q "$BBX_MNTDIR/$ifc"; then
-      echo "Abort, interface '$DCKR_MNT/$ifc' not bound."
+      echo "Abort, interface '$BBX_MNTDIR/$ifc' not bound."
       return 1
     fi
   done
@@ -12,8 +12,19 @@ isoption() {
   [ "$(echo "$1" | cut -c -2)" = '--' ]
 }
 
-listtasks() {
+tasklist() {
   if test -d "$BBX_TASKDIR"; then
     (cd "$BBX_TASKDIR" && find -L -maxdepth 1 -type f -printf "%f\n" | sort -u)
   fi
+}
+
+cpucount() {
+  grep -m 1 '^Cpus_allowed:' /proc/self/status |
+  cut -f 2 |
+  tr -d ',' |
+  tr '[:lower:]' '[:upper:]' |
+  xargs echo "obase=2; ibase=16;" |
+  bc |
+  tr -d -c '1' |
+  wc -c
 }
