393301245ddcddcd7c19ce478b2bd4ce13bfbd01
Johannes Dröge <fungs@users.noreply.github.com>
2015-06-25 16:03:01 +0200

    refactor mountcheck as bourne function

---
+++ dckr/lib/run-library.sh
@@ -0,0 +1,10 @@
+mountcheck() {
+  grep -v '^#' "$BBX_MNTCONF" |
+  while read ifc; do
+    if ! mountpoint -q "$BBX_MNTDIR/$ifc"; then
+      echo "Abort, interface '$DCKR_MNT/$ifc' not bound." 1>&2
+      return 1
+    fi
+  done
+}
+
